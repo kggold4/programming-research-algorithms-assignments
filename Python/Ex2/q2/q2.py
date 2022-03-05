b1395c1829a7f1d4ac93cef88cd2c01d348da5b3
kggold4 <68167734+kggold4@users.noreply.github.com>
2022-03-06 01:00:08 +0200

    change all repo to one proj for all assignment

---
+++ Python/Ex2/q2/q2.py
@@ -0,0 +1,35 @@
+# global last value
+last_value = None
+
+
+def lastcall(func):
+    def inner(arg):
+        global last_value
+        if not (last_value is None or arg != last_value):
+            print(f"I already told you the the answer is {func(arg)}!")
+        else:
+            last_value = arg
+        return func(arg)
+
+    return inner
+
+
+@lastcall
+def f(x: int):
+    return x ** 2
+
+
+@lastcall
+def g(s: str):
+    return s + "-string"
+
+
+if __name__ == '__main__':
+    f(2)
+    f(2)
+    f(2)
+    f(2)
+    f(3)
+    f(3)
+    g("hello")
+    g("hello")
