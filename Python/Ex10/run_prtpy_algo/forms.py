f5dab97493023c115473906469e740f1389e9d9d
kggold4 <68167734+kggold4@users.noreply.github.com>
2022-06-09 15:26:27 +0300

    done website

--- Python/Ex10/run_prtpy_algo/forms.py
+++ Python/Ex10/run_prtpy_algo/forms.py
@@ -2,9 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import SubmitField, StringField, IntegerField, SelectField
 from wtforms.validators import DataRequired, InputRequired, Length, Regexp, NumberRange
 
+from run_prtpy_algo.objects.algorithm_response import get_algorithms_choices
+
 
 class InputForm(FlaskForm):
-    items = StringField('Items', validators=[DataRequired(), Regexp("^[1-9](,[1-9])*$")])
+    items = StringField('Items', validators=[DataRequired(), Regexp("\d+(?:,\d+)?")])
     num_of_bins = IntegerField('Number of bins', validators=[InputRequired(), NumberRange(min=0, max=20)])
-    algorithm = SelectField('Algorithm', choices=[('kk', 'kk'), ('ckk', 'ckk'), ('rnp', 'rnp'), ('irnp', 'irnp')])
+    algorithm = SelectField('Algorithm', choices=get_algorithms_choices())
     submit = SubmitField('Submit')
--- Python/Ex10/run_prtpy_algo/routes.py
+++ Python/Ex10/run_prtpy_algo/routes.py
@@ -1,15 +1,10 @@
 import json
 
-from flask import render_template, redirect, url_for, request, session
-
-from prtpy.partitioning.ckk import ckk
-from prtpy.partitioning.irnp import irnp
-from prtpy.partitioning.kk import kk
-from prtpy.partitioning.rnp import rnp
+from flask import render_template, redirect, url_for, request
 from run_prtpy_algo import app
 from run_prtpy_algo.forms import InputForm
-from run_prtpy_algo.static.my_objects.algorithm_request import AlgorithmRequest
-from run_prtpy_algo.static.my_objects.algorithm_response import AlgorithmResponse
+from run_prtpy_algo.objects.algorithm_request import AlgorithmRequest
+from run_prtpy_algo.utils import get_input_items, get_algorithm_response
 
 
 @app.route('/')
@@ -17,40 +12,18 @@ def index():
     return render_template('index.html')
 
 
-def _get_input_items(items: str) -> list:
-    return [int(number) for number in items.split(',')]
-
-
-def _get_algorithm_by_name(name: str):
-    if name == "kk":
-        return kk
-    elif name == "ckk":
-        return ckk
-    elif name == "rnp":
-        return rnp
-    elif name == "irnp":
-        return irnp
-    else:
-        raise ValueError(f"Unknown algorithm named: {name}")
-
-
-def _get_algorithm_response(algo_request):
-    algorithm = _get_algorithm_by_name(algo_request.get('algorithm'))
-    items = algo_request.get('items')
-    num_of_bins = int(algo_request.get('num_of_bins'))
-    from prtpy import partition
-    result_bins = partition(algorithm=algorithm, numbins=num_of_bins, items=items)
-    return AlgorithmResponse(bins=result_bins, sums=[sum(s) for s in result_bins])
-
-
 @app.route('/input', methods=['GET', 'POST'])
 def input_form():
     form = InputForm()
     is_submitted = form.validate_on_submit()
+
+    # input form not completed
     if not is_submitted:
         return render_template('input.html', form=form)
+
+    # get algorithm request and sent it by json format
     else:
-        algo_request = AlgorithmRequest(items=_get_input_items(form.items.data),
+        algo_request = AlgorithmRequest(items=get_input_items(form.items.data),
                                         num_of_bins=int(form.num_of_bins.data),
                                         algorithm=form.algorithm.data)
         return redirect(url_for(show_results.__name__, algo_request=algo_request.to_json()))
@@ -58,7 +31,7 @@ def input_form():
 
 @app.route('/results', methods=['GET', 'POST'])
 def show_results():
+    # convert the algorithm request from json to object and get response
     algo_request = request.args['algo_request']
-    algo_response = _get_algorithm_response(json.loads(algo_request))
-    print(algo_response)
+    algo_response = get_algorithm_response(json.loads(algo_request))
     return render_template('results.html', algo_response=algo_response)
